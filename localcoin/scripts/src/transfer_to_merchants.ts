db3e9afb29e96f68e5a7c0a85b9f5e9c1ffd2e17
suyogadhikari <adhikarisuyog49@gmail.com>
2024-05-27 21:30:53 +0545

    test: parameter modified in the script.

--- localcoin/scripts/src/transfer_to_merchants.ts
+++ localcoin/scripts/src/transfer_to_merchants.ts
@@ -1,6 +1,9 @@
 import { TransactionBlock } from '@mysten/sui.js/transactions';
 import * as dotenv from 'dotenv';
 import getExecStuff from '../utils/execstuff';
+import fs from 'fs';
+import path from 'path';
+import sleep from '../utils/sleep'
 dotenv.config();
 
 
@@ -17,6 +20,7 @@ async function merchantTransfer() {
     const pt = tx.moveCall({
         target: `${packageId}::local_coin::transfer_token_to_merchants`,
         arguments: [
+            tx.pure.u64(1_000_000),
             tx.pure.address(merchantAddress),
             // local coin token
             tx.object(localCoinToken),
@@ -29,6 +33,40 @@ async function merchantTransfer() {
         transactionBlock: tx,
     });
     console.log({ result });
+    const digest_ = result.digest;
+    await sleep(40000);
+
+    const txn = await client.getTransactionBlock({
+        digest: String(digest_),
+        // only fetch the effects and objects field
+        options: {
+            showEffects: true,
+            showInput: false,
+            showEvents: false,
+            showObjectChanges: true,
+            showBalanceChanges: false,
+        },
+    });
+    let output: any;
+    output = txn.objectChanges;
+
+    let local_coin;
+
+    for (let i = 0; i < output.length; i++) {
+        const item = output[i];
+        if (await item.type === 'created') {
+            if (await item.objectType === `0x2::token::Token<${packageId}::local_coin::LOCAL_COIN>`) {
+                local_coin = String(item.objectId);
+            }
+        }
+    }
+    console.log(local_coin);
+    const envFilePath = path.resolve(__dirname, '../../.env');
+    let envData = fs.readFileSync(envFilePath, 'utf8');
+
+    envData = envData.replace(/^LC_TOKEN_MERCHANT\s*=\s*.*/m, `LC_TOKEN_MERCHANT='${local_coin}'`);
+    fs.writeFileSync(envFilePath, envData);
+
 }
 
 
